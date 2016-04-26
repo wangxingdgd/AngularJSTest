21ef4e94bc94a9fcd1819ef43318f00f4d17dc1f
wangxingdgd <285493005@qq.com>
2016-04-26 15:49:10 +0800

    修改

--- js/myNoteCtrl.js
+++ js/myNoteCtrl.js
@@ -9,19 +9,19 @@ app.controller("myNoteCtrl", function($scope) {
     	if($scope.num1 && $scope.num2){
 	    	if($scope.biz){
 	    		if($scope.biz == "1"){
-	    			$scope.add();
+	    			return $scope.add();
 	    		}
 	    		if($scope.biz == "2"){
-	    			$scope.subtract();
+	    			return $scope.subtract();
 	    		}
 	    		if($scope.biz == "3"){
-	    			$scope.multiplication();
+	    			return $scope.multiplication();
 	    		}
 	    		if($scope.biz == "4"){
 	    			if($scope.num2 == "0"){
 	    				$scope.errorMsg = "不能被0除";
 	    			}else{
-	    				$scope.division();
+	    				return $scope.division();
 	    			}
 	    		}
 	    	}else{
@@ -32,15 +32,15 @@ app.controller("myNoteCtrl", function($scope) {
     	}
     };
     $scope.add = function(){
-    	$scope.num3 = Number($scope.num1) + Number($scope.num2);
+    	return Number($scope.num1) + Number($scope.num2);
     }
     $scope.subtract = function(){
-    	$scope.num3 = Number($scope.num1) - Number($scope.num2);
+    	return Number($scope.num1) - Number($scope.num2);
     }
     $scope.multiplication = function(){
-    	$scope.num3 = Number($scope.num1) * Number($scope.num2);
+    	return Number($scope.num1) * Number($scope.num2);
     }
     $scope.division = function(){
-    	$scope.num3 = Number($scope.num1) / Number($scope.num2);
+    	return Number($scope.num1) / Number($scope.num2);
     }
 });
